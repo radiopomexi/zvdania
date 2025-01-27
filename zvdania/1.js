d421def4f81a961f467c57a23c89ca6b325e29f2
radiopomexi <git.radiopomexi@gmail.com>
2025-01-27 13:18:13 +0900

    n1

---
+++ zvdania/1.js
@@ -0,0 +1,22 @@
+// --- 1 --- //
+const Myname = "дима";
+const programmingLanguage = "java script";
+const CourseCreatorName = "Никита Михайлович";
+const ReasonText = "не знаю";
+const numberoffMoth = "маленько";
+const result1 =
+  "Всем привет, меня зовут " +
+  Myname +
+  ", сейчас я изучаю язык программирования " +
+  programmingLanguage +
+  " на курсе по " +
+  programmingLanguage +
+  " у " +
+  CourseCreatorName +
+  ", Я хочу стать веб разработчиком, потому что " +
+  ReasonText +
+  ", до этого я изучал " +
+  programmingLanguage +
+  numberoffMoth +
+  " месяцев. Я (не) уверен что пройду этот курс до конца.";
+console.log(result1);
